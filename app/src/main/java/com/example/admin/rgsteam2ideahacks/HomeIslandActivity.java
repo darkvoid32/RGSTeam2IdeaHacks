d8c14de0267378e87831e5701901dac5b190c77e
Zhuo Quan <boyboystudies@gmail.com>
2017-11-11 12:28:12 +0800

    Add Home

---
+++ app/src/main/java/com/example/admin/rgsteam2ideahacks/HomeIslandActivity.java
@@ -0,0 +1,16 @@
+package com.example.admin.rgsteam2ideahacks;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Zhuo Quan on 11/11/2017.
+ */
+
+public class HomeIslandActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_island);
+    }
+}
