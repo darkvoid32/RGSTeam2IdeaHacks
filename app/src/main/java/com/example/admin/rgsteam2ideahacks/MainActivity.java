4bfe4e246776e41373157281ddc24502cd1cb6ab
Admin <h1410115@nushigh.edu.sg>
2017-11-11 14:13:50 +0800

    Added horizontal side view, islands working now

--- app/src/main/java/com/example/admin/rgsteam2ideahacks/MainActivity.java
+++ app/src/main/java/com/example/admin/rgsteam2ideahacks/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.admin.rgsteam2ideahacks;
 
+import android.app.ActionBar;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView mainIV, mathIV, physicsIV;
+    private HorizontalScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // Remove title
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // Full Screen
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
 
         //TODO Home page with bird eye view of different islands have clouds floating around
         //TODO Main islands (Graphics)
@@ -18,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
         //TODO Achievement shit (Later)
         //TODO Level shit (Later)
 
-
+        setUpIslandIV();
     }
+
+    private void setUpIslandIV() {
+        mainIV = (ImageView) findViewById(R.id.mainIsland);
+        mathIV = (ImageView) findViewById(R.id.sideIslandMathIV);
+        physicsIV = (ImageView) findViewById(R.id.sideIslandPhysicsIV);
+
+        scrollView = (HorizontalScrollView) findViewById(R.id.rootScrollView);
+
+        ViewTreeObserver vto = scrollView.getViewTreeObserver(); // Setting Horizontal scrollView to goto middle (Main island)
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            public void onGlobalLayout() {
+                scrollView.scrollTo(scrollView.getChildAt(0).getWidth()/3, 0);
+            }
+        });
+    }
+
 }
