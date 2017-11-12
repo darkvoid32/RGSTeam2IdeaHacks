fc2ce1bae1e75f9d3c6e9dfdedf9e2535e220bb3
Zhuo Quan <boyboystudies@gmail.com>
2017-11-12 11:55:58 +0800

    lol

--- app/src/main/java/com/example/admin/rgsteam2ideahacks/HomeIslandActivity.java
+++ app/src/main/java/com/example/admin/rgsteam2ideahacks/HomeIslandActivity.java
@@ -11,6 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
