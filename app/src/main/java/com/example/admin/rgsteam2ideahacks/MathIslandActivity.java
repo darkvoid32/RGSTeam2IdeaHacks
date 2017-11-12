d4ea9b8a95b45d3ad6a4a74e3ac3d78ab6f5072e
OrangeNg <ngquanhao@hotmail.com>
2017-11-12 11:33:12 +0800

    Updated how each activity gets the Player object.

--- app/src/main/java/com/example/admin/rgsteam2ideahacks/MathIslandActivity.java
+++ app/src/main/java/com/example/admin/rgsteam2ideahacks/MathIslandActivity.java
@@ -3,10 +3,17 @@ package com.example.admin.rgsteam2ideahacks;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by Gnoh Cheng Yi on 11/11/2017.
@@ -31,6 +38,35 @@ public class MathIslandActivity extends AppCompatActivity{
         btn_geo = findViewById(R.id.btn_geo);
         btn_nt = findViewById(R.id.btn_nt);
         intent = new Intent(getApplicationContext(), DescriptionActivity.class);
+
+        try{
+            FileInputStream inputStream = openFileInput("UserAccounts.txt");
+            Scanner inputStreamScanner = new Scanner(inputStream);
+
+            ArrayList<String> lines  = new ArrayList<String>();
+            while(inputStreamScanner.hasNextLine()){
+                lines.add(inputStreamScanner.nextLine());
+            }
+
+            player = new Player(lines.get(0), Integer.parseInt(lines.get(1)), Integer.parseInt(lines.get(2)));
+
+            // Parse physics progress into array
+            Scanner physicsScanner = new Scanner(lines.get(3));
+            physicsScanner.useDelimiter(",");
+            ArrayList<Boolean> physicsProgress = new ArrayList<>();
+
+            while(physicsScanner.hasNext()){
+                physicsProgress.add(new Boolean(physicsScanner.next()));
+            }
+
+            player.setPhysicsProgress(physicsProgress);
+        }
+        catch(IOException exception){
+            Log.w("MainActivity", exception.getStackTrace().toString());
+        }
+
+        Toast.makeText(this, player.toString(), Toast.LENGTH_LONG).show();
+
     }
 
     public void onAlgebraClick(View v){
